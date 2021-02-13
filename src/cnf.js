25367ff9d9b0439741dd4793df172dd678e211da
Oxygemo <alej0hio2007@gmail.com>
2021-02-13 11:45:15 +0100

    rebrand and remove husky
    Signed-off-by: Oxygemo <alej0hio2007@gmail.com>

--- src/cnf.js
+++ src/cnf.js
@@ -1,7 +1,7 @@
 const client = require("../cybase");
 
 const config = {
-  "botName": "Cytrus-RE", // Name of bot. Don't change this.
+  "botName": "Delta", // Name of bot. Don't change this.
   // NOTE: The IDs are sorted by the comments. So, Devnol is the first ID in "managers", and then comes... well, we don't have any other managers yet but yeah.
   "owners": ["395990735934980097"], // Rexo has full power ahahahaha
   "managers": ["429353559566319626", "746149573613060176"], // Devnol & Midou
@@ -21,11 +21,11 @@ const config = {
   "botInvite": "https://terrific-tea.github.io/links/cyinvite",
 
   "defaultSettings" : {
-    "prefix": "c.", // Default prefix
+    "prefix": "d.", // Default prefix
     "modLogChannel": "log", // default logging channel
     "modRole": "Moderator",
     "adminRole": "Administrator",
-    "muteRole": "Cytrus-RE Mute",
+    "muteRole": "Delta Mute",
     "noPermissionNotice": "true",
     "welcomeChannel": "off-topic",
     "welcomeMessage": "Welcome to the server, {{ping}}!",
