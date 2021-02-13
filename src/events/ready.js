83fd8a8d87d23e0063f5ee6005abe9d9f2633870
DestroyedEDGE <44955931+DestroyedEDGE@users.noreply.github.com>
2021-02-13 05:09:21 -0700

    removed references to the website because we dont have one

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,19 +1,11 @@
 module.exports = async client => {
   const statusList = [
-    {msg: `Cytrus-RE 1.3.0 | ${client.config.defaultSettings.prefix}help | Check out our ${client.config.defaultSettings.prefix}site!`, type: "PLAYING"},
-    {msg: `for commands | ${client.config.defaultSettings.prefix}help | Check out our ${client.config.defaultSettings.prefix}site!`, type: "WATCHING"},
-    //{msg: `with async/await errors | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'PLAYING'},
-    //{msg: `with unhandled promise rejections | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'PLAYING'},
-    //{msg: `with linux permissions | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'PLAYING'},
-    //{msg: `Discord be slow | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'WATCHING'},
-    {msg: `over ${client.guilds.cache.size} servers | ${client.config.defaultSettings.prefix}help | Check out our ${client.config.defaultSettings.prefix}site!`, type: "WATCHING"},
-    {msg: `development progress! | ${client.config.defaultSettings.prefix}help | Check out our ${client.config.defaultSettings.prefix}site!`, type: "WATCHING"},
-    //{msg: `with Git and learning at the same time! | ${client.config.defaultSettings.prefix}help | cytrus-re.github.io`, type: 'PLAYING'},
+    {msg: `Delta v1.3.0 | ${client.config.defaultSettings.prefix}help`, type: "PLAYING"},
+    {msg: `for commands | ${client.config.defaultSettings.prefix}help`, type: "WATCHING"},
+    {msg: `over ${client.guilds.cache.size} servers | ${client.config.defaultSettings.prefix}help`, type: "WATCHING"},
+    {msg: `development progress! | ${client.config.defaultSettings.prefix}help`, type: "WATCHING"},
   ];
-    // Why did I comment out the rest? 
-    // If we're gonna try to be a serious bot, then we need to have serious status messages. 
-    // "playing with linux permissions" this comes from a kid who's too scared to use linux but ok
-    // "with Git" sorry what - odyssey
+
   setInterval(async () => {
     let index = Math.floor(Math.random() * statusList.length + 1) - 1;
     await client.user.setActivity(statusList[index].msg, {
@@ -36,7 +28,7 @@ module.exports = async client => {
   client.logger.info(`Running on Node ${process.version.replace(" ", "")}`, "ready");
   client.logger.info(`...and using Discord.js v${require("discord.js").version.replace(" ", "")}`, "ready");
 
-  client.logger.info("Cytrus-RE V" + require("../../package").version + " | https://github.com/Cytrus-RE/cytrus-re");
+  client.logger.info("Delta v" + require("../../package").version + " | https://github.com/DestroyedEDGE/delta2");
   client.startuptime = new Date().getTime() - client.starttime;
-  client.logger.info("It took " + client.startuptime + "ms to start Cytrus-RE.");
+  client.logger.info("It took " + client.startuptime + "ms to start Delta.");
 };
