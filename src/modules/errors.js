b8c3965e787d697b5a4bef567f6c330fec2831a8
Oxygemo <alej0hio2007@gmail.com>
2021-02-13 13:14:47 +0100

    oops forgot to save
    Signed-off-by: Oxygemo <alej0hio2007@gmail.com>

--- src/modules/errors.js
+++ src/modules/errors.js
@@ -1,6 +1,6 @@
 const errors = {
   "genericError": "❌ There was an error!\nPlease send the following to a Delta dev (Odyssey346#9848 or ƎƃᗡƎpǝʎoɹʇsǝᗡ#0120) and tell him what command you used (including arguments).\n---------------------------------------------------------------------------------------------------------------------\n",
-  "fancyError": "Something went wrong with the bot\nPlease send this to a Delta developer (Odyssey346#9848 or ƎƃᗡƎpǝʎoɹʇsǝᗡ#0120) through a DM.\n",
+  "fancyError": "Something went wrong with the bot.\nPlease send this to a Delta developer (Odyssey346#9848 or ƎƃᗡƎpǝʎoɹʇsǝᗡ#0120) through a DM.\n",
   "wiiError": "❌ An error has occurred.\nPress the Eject button, remove the Game Disc, and turn off the power to the console.\nPlease read the Wii Operations Manual for further instructions.",
   "userNotInGuild": "❌ This user isn't in this guild!",
   "cannotBanSelf":  "❌ Sorry, you can't ban yourself. (It would be funny, if it weren't so sad.)",
