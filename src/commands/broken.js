29b0918b5ec18edb86f08980da0d4d7f547a7d5d
Oxygemo <alej0hio2007@gmail.com>
2021-02-13 12:30:01 +0100

    shit
    Signed-off-by: Oxygemo <alej0hio2007@gmail.com>

---
+++ src/commands/broken.js
@@ -0,0 +1,43 @@
+const Discord = require("discord.js");
+const os = require("os");
+
+exports.run = async (client, message, args, level) => { 
+  try {
+    var opsys = os.platform();
+    var cleanOS = client.friendlyOS(opsys);
+    const embed = new Discord.MessageEmbed()
+    .setColor("#eeeeee")
+    .setTitle("Cytrus-RE")
+    .setFooter("Originally made by CelestialCrafter and EnderGirlGamer. Rewritten by Devnol, Rexowogamer, Odyssey346 and Midou.")
+    .setDescription(`Github: [Repo](${client.config.github})
+Website: [Check it out](${client.config.site})
+Support Server: [Join](${client.config.supportServer})
+Issues: [Right here](${client.config.github}/issues)
+Version: V1.3
+Currently running on: ${clenOS}`);
+    message.channel.send(embed);
+  } catch (err) {
+    const embed = new Discord.MessageEmbed()
+    .setColor("#FF0000")
+    .setTitle("Something went wrong")
+    .setFooter(client.config.botName, "v1.3.0-delta")
+    .setDescription(`Something went wrong with the bot.
+${client.errors.genericError}
+${err}`);
+    message.channel.send(embed);
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  aliases: ["i"],
+  guildOnly: false,
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "info",
+  category: "General",
+  description: "Returns info about Cytrus-RE.",
+  usage: "info"
+};
