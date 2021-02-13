0733c93e20c6196f5170f13b2069d040940b4430
Oxygemo <alej0hio2007@gmail.com>
2021-02-13 12:31:54 +0100

    asd12342344234
    Signed-off-by: Oxygemo <alej0hio2007@gmail.com>

--- src/commands/broken.js
+++ src/commands/broken.js
@@ -21,9 +21,9 @@ Currently running on: ${clenOS}`);
     .setColor("#FF0000")
     .setTitle("Something went wrong")
     .setFooter(client.config.botName, "v1.3.0-delta")
-    .setDescription(`Something went wrong with the bot.
-${client.errors.genericError}
-${err}`);
+    .setDescription(`${client.errors.genericError}
+${err}
+What can I do?\nYou can only report the error. We plan on making it automatically report errors in the future.`);
     message.channel.send(embed);
   }
 };
