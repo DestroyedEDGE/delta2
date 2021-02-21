fc95fe67f8c1cffbca671e57d9d2aab26da088f0
Oxygemo <alej0hio2007@gmail.com>
2021-02-21 14:13:43 +0100

    the extremely modular discord bot just got more modular.
    Signed-off-by: Oxygemo <alej0hio2007@gmail.com>

--- src/commands/unban.js
+++ src/commands/unban.js
@@ -29,7 +29,7 @@ exports.run = async (client, message, args) => {
     const embed = new Discord.MessageEmbed()
     .setColor("#FF0000")
     .setTitle("Something went wrong")
-    .setFooter(`${client.config.botName} v1.3.0-delta`)
+    .setFooter(`${client.config.botName} v${client.config.version}`)
     .addField("Info", `${client.errors.fancyError}`)
     .addField("What's the error?", `${err}`)
     .addField("What can I do?", `You can only report the error. We plan on making it automatically report errors in the future.`);
